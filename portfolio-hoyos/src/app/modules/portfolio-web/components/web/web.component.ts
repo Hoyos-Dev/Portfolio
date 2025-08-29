46846bf7ccf1dfae472993d8b8804ce6597030ec
Christian Hoyos Salazar <chsalazar@REDCOLSA.LOCAL>
2025-08-29 16:42:50 -0500

    Segunda Seccion

--- portfolio-hoyos/src/app/modules/portfolio-web/components/web/web.component.ts
+++ portfolio-hoyos/src/app/modules/portfolio-web/components/web/web.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-web',
   templateUrl: './web.component.html',
   styleUrls: ['./web.component.scss']
 })
-export class WebComponent {
+export class WebComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit(): void {
+    // Esperar 10 segundos y luego hacer scroll a la segunda sección
+    setTimeout(() => {
+      const section2 = document.getElementById('section2');
+      if (section2) {
+        section2.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 1000); //Tiempo vista dos
+  }
 
 }
